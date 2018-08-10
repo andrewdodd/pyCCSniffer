fe2b3ad7ea4c4ddd6f0fd7d83318e9717fdb5fe1
Andrew Dodd <andrew@adimian.com>
2018-08-10 13:10:50 +0200

    YAPF the file

--- pyCCSniffer.py
+++ pyCCSniffer.py
@@ -45,8 +45,6 @@ import usb.core
 import usb.util
 
 import ieee15dot4 as ieee
-
-
 """
    Functionality
    -------------
@@ -95,6 +93,7 @@ class SniffedPacket(object):
     def get_macPDU(self):
         return self.__macPDUByteArray
 
+
 class CapturedFrame(object):
     def __init__(self, frame, rssiSniff, annotation):
         self.frame = frame
@@ -103,11 +102,11 @@ class CapturedFrame(object):
 
     def __repr__(self, *args, **kwargs):
         if len(self.annotation) > 0:
-            return "{} RssiSniff[{}] Annotation[{}]".format(self.frame, 
-                                                            self.rssiSniff,
-                                                            self.annotation)
-        
+            return "{} RssiSniff[{}] Annotation[{}]".format(
+                self.frame, self.rssiSniff, self.annotation)
+
         return "{} RssiSniff[{}]".format(self.frame, self.rssiSniff)
+
 
 class CustomAssertFrame(object):
     def __init__(self, date, code, line, file, **kwargs):
@@ -118,12 +117,14 @@ class CustomAssertFrame(object):
         self.time = datetime.now()
 
     def __repr__(self, *args, **kwargs):
-        return "AssertFrame Time[{}] Code[{}] Line[{}] File[{}] Compiled[{}]".format(self.time, self.code, self.line, self.file, self.date)
-    
+        return "AssertFrame Time[{}] Code[{}] Line[{}] File[{}] Compiled[{}]".format(
+            self.time, self.code, self.line, self.file, self.date)
+
+
 class PacketHandler(object):
     def __init__(self):
         stats['Dissected'] = 0
-        stats["Dissection errors"]  = 0
+        stats["Dissection errors"] = 0
         stats["CRC Errors"] = 0
         stats["Beacon"] = 0
         stats["Data"] = 0
@@ -145,29 +146,30 @@ class PacketHandler(object):
     def enable(self):
         logger.info("Dissector enabled")
         self.__enabled = True
+
     def disable(self):
         logger.info("Dissector disabled")
         self.__enabled = False
+
     def isEnabled(self):
         return self.__enabled
-    
+
     def setAnnotation(self, annotation):
         self.__annotation = annotation
 
-        
     def printAllFrames(self):
         print("Printing all captures")
-        print("-"*40)
+        print("-" * 40)
         for capture in self.captures:
             print(capture)
-    
-        print("-"*40)
+
+        print("-" * 40)
         sys.stdout.flush()
-        
+
     @staticmethod
     def handleCustomFrames(sniffedPacket):
         pdu = sniffedPacket.get_macPDU()
-        (debugString,) = struct.unpack_from("<5s", pdu, 0)
+        (debugString, ) = struct.unpack_from("<5s", pdu, 0)
 
         # For example, I have implemented a generic debug packet that gets sent
         # whenever there is an assert in my code. It takes control of the 
@@ -182,22 +184,23 @@ class PacketHandler(object):
                 #                'Debug', version,  'date12', Line num, code, fcs
                 #                 12345 ,  6     ,   789012 , 34      , 5
                 nameLength = len(pdu) - 5 - 1 - 6 - 2 - 1 - 2
-                (fileName,) = struct.unpack_from("<%ds" % nameLength, pdu, 15)
-                
+                (fileName, ) = struct.unpack_from("<%ds" % nameLength, pdu, 15)
+
                 return CustomAssertFrame(date, code, lineNum, fileName)
 
-        return None # the frame was NOT consumed
-          
+        return None  # the frame was NOT consumed
+
     def handleSniffedPacket(self, sniffedPacket):
         if self.__enabled is False:
             return
 
         try:
-            if (None == sniffedPacket) or (len(sniffedPacket.get_macPDU()) < 2):
+            if (None == sniffedPacket) or (
+                    len(sniffedPacket.get_macPDU()) < 2):
                 return
 
-
-            (rssiSniff, corr, crc_ok) = self.checkPacket(sniffedPacket.get_macPDU())
+            (rssiSniff, corr,
+             crc_ok) = self.checkPacket(sniffedPacket.get_macPDU())
 
             if crc_ok is False:
                 stats["CRC Errors"] += 1
@@ -206,24 +209,25 @@ class PacketHandler(object):
             customFrame = self.handleCustomFrames(sniffedPacket)
             if customFrame is not None:
                 # A custom, non-802.15.4 frame was received and processed
-                capture = CapturedFrame(customFrame, rssiSniff, self.__annotation)
+                capture = CapturedFrame(customFrame, rssiSniff,
+                                        self.__annotation)
                 self.captures.append(capture)
                 print(capture)
                 sys.stdout.flush()
-                
+
             else:
                 frame = ieee.IEEE15dot4FrameFactory.parse(sniffedPacket)
                 capture = CapturedFrame(frame, rssiSniff, self.__annotation)
-                
+
                 if capture is not None:
                     self.captures.append(capture)
                     print(capture)
                     # hack here!
                     sys.stdout.flush()
-    
+
                 stats[ieee.FrameType.toString(frame.fcf.frametype)] += 1
                 stats['Dissected'] += 1
-            
+
         except Exception as e:
             logger.warn("Error dissecting frame.")
             logger.warn("The error was: %s" % (e.args))
@@ -235,16 +239,15 @@ class PacketHandler(object):
         fcs1, fcs2 = packet[-2:]
 
         # rssi is the signed value at fcs1
-        rssi    = (fcs1 + 2**7) % 2**8 - 2**7  - 73
+        rssi = (fcs1 + 2**7) % 2**8 - 2**7 - 73
 
         # crc ok is the 7th bit in fcs2
-        crc_ok  = fcs2 & (1 << 7) > 0
+        crc_ok = fcs2 & (1 << 7) > 0
 
         # correlation value is the unsigned 0th-6th bit in fcs2
-        corr    = fcs2 & 0x7f
+        corr = fcs2 & 0x7f
 
         return (rssi, corr, crc_ok)
-
 
 
 class CC2531EMK:
@@ -267,7 +270,8 @@ class CC2531EMK:
     SET_CHAN = 0xd2  # 0x0d (idx 0) + data)0x00 (idx 1)
 
     COMMAND_FRAME = 0x00
-#     COMMAND_CHANNEL = ??
+
+    #     COMMAND_CHANNEL = ??
 
     def __init__(self, callback, channel=DEFAULT_CHANNEL):
         """Create a new CC2531EMK manager object
@@ -279,7 +283,7 @@ class CC2531EMK:
                             with a signature (timestamp, frame).
             channel(int): The channel to sniff on.
         """
-        
+
         self.dev = None
         self.channel = channel
         self.callback = callback
@@ -288,16 +292,18 @@ class CC2531EMK:
 
         stats['Captured'] = 0
         stats['Non-Frame'] = 0
-        
+
         if self.callback is None:
             raise ValueError("A valid callback must be provided")
         if not is_callback_valid(self.callback):
             raise ValueError("Callback must have at least 2 arguments")
-        
+
         try:
             self.dev = usb.core.find(idVendor=0x0451, idProduct=0x16ae)
         except usb.core.USBError:
-            raise OSError("Permission denied, you need to add an udev rule for this device", errno=errno.EACCES)
+            raise OSError(
+                "Permission denied, you need to add an udev rule for this device",
+                errno=errno.EACCES)
 
         if self.dev is None:
             raise IOError("Device not found")
@@ -310,11 +316,13 @@ class CC2531EMK:
             256)  # get identity from Firmware command
 
         # power on radio, wIndex = 4
-        self.dev.ctrl_transfer(CC2531EMK.DIR_OUT, CC2531EMK.SET_POWER, wIndex=4)
+        self.dev.ctrl_transfer(
+            CC2531EMK.DIR_OUT, CC2531EMK.SET_POWER, wIndex=4)
 
         while True:
             # check if powered up
-            power_status = self.dev.ctrl_transfer(CC2531EMK.DIR_IN, CC2531EMK.GET_POWER, 0, 0, 1)
+            power_status = self.dev.ctrl_transfer(CC2531EMK.DIR_IN,
+                                                  CC2531EMK.GET_POWER, 0, 0, 1)
             if power_status[0] == 4: break
             time.sleep(0.1)
 
@@ -346,8 +354,9 @@ class CC2531EMK:
 
         # While the running flag is set, continue to read from the USB device
         while self.running:
-            bytesteam = self.dev.read(CC2531EMK.DATA_EP, 4096, timeout=CC2531EMK.DATA_TIMEOUT)
-#             print("RECV>> %s" % binascii.hexlify(bytesteam))
+            bytesteam = self.dev.read(
+                CC2531EMK.DATA_EP, 4096, timeout=CC2531EMK.DATA_TIMEOUT)
+            #             print("RECV>> %s" % binascii.hexlify(bytesteam))
 
             if len(bytesteam) >= 3:
                 (cmd, cmdLen) = struct.unpack_from("<BH", bytesteam)
@@ -355,15 +364,18 @@ class CC2531EMK:
                 if len(bytesteam) == cmdLen:
                     # buffer contains the correct number of bytes
                     if CC2531EMK.COMMAND_FRAME == cmd:
-                        logger.info('Read a frame of size %d' % (cmdLen,))
+                        logger.info('Read a frame of size %d' % (cmdLen, ))
                         stats['Captured'] += 1
-                        (timestamp, pktLen) = struct.unpack_from("<IB", bytesteam)
+                        (timestamp, pktLen) = struct.unpack_from("<IB",
+                                                                 bytesteam)
                         frame = bytesteam[5:]
 
                         if len(frame) == pktLen:
                             self.callback(timestamp, frame)
                         else:
-                            logger.warn("Received a frame with incorrect length, pkgLen:%d, len(frame):%d" %(pktLen, len(frame)))
+                            logger.warn(
+                                "Received a frame with incorrect length, pkgLen:%d, len(frame):%d"
+                                % (pktLen, len(frame)))
                             stats['Non-Frame'] += 1
 
 #                     elif cmd == CC2531EMK.COMMAND_CHANNEL:
@@ -373,7 +385,6 @@ class CC2531EMK:
 #                         print('Sniffing in channel: %d' % (bytesteam[0],))
 #                     else:
 #                         logger.warn("Received a command response with unknown code - CMD:%02x byte:%02x]" % (cmd, bytesteam[0]))
-
 
     def set_channel(self, channel):
         was_running = self.running
@@ -385,8 +396,10 @@ class CC2531EMK:
             self.channel = channel
 
             # set channel command
-            self.dev.ctrl_transfer(CC2531EMK.DIR_OUT, CC2531EMK.SET_CHAN, 0, 0, [channel])
-            self.dev.ctrl_transfer(CC2531EMK.DIR_OUT, CC2531EMK.SET_CHAN, 0, 1, [0x00])
+            self.dev.ctrl_transfer(CC2531EMK.DIR_OUT, CC2531EMK.SET_CHAN, 0, 0,
+                                   [channel])
+            self.dev.ctrl_transfer(CC2531EMK.DIR_OUT, CC2531EMK.SET_CHAN, 0, 1,
+                                   [0x00])
 
             self.get_channel()
 
@@ -405,61 +418,83 @@ class CC2531EMK:
         else:
             return "Not connected"
 
+
 def arg_parser():
     debug_choices = ('DEBUG', 'INFO', 'WARN', 'ERROR')
 
-    parser = argparse.ArgumentParser(add_help = False,
-                                     description = 'Read IEEE802.15.4 frames \
-    from a CC2531EMK packet sniffer device, parse them and dispay them in text.')
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        description='Read IEEE802.15.4 frames \
+    from a CC2531EMK packet sniffer device, parse them and dispay them in text.'
+    )
 
     in_group = parser.add_argument_group('Input Options')
-    in_group.add_argument('-c', '--channel', type = int, action = 'store',
-                          choices = range(11, 27),
-                          default = defaults['channel'],
-                          help = 'Set the sniffer\'s CHANNEL. Valid range: 11-26. \
-                                  (Default: %s)' % (defaults['channel'],))
-    in_group.add_argument('-a', '--annotation', type = str,
-                          help = 'Include a free-form annotation on every capture.')
+    in_group.add_argument(
+        '-c',
+        '--channel',
+        type=int,
+        action='store',
+        choices=range(11, 27),
+        default=defaults['channel'],
+        help='Set the sniffer\'s CHANNEL. Valid range: 11-26. \
+                                  (Default: %s)' % (defaults['channel'], ))
+    in_group.add_argument(
+        '-a',
+        '--annotation',
+        type=str,
+        help='Include a free-form annotation on every capture.')
 
     log_group = parser.add_argument_group('Verbosity and Logging')
-    log_group.add_argument('-r', '--rude',
-                           action = 'store_true',
-                           default = False,
-                           help = 'Run in non-interactive mode, without \
+    log_group.add_argument(
+        '-r',
+        '--rude',
+        action='store_true',
+        default=False,
+        help='Run in non-interactive mode, without \
                                    accepting user input. (Default Disabled)')
-    log_group.add_argument('-D', '--debug-level',
-                           action = 'store',
-                           choices = debug_choices,
-                           default = defaults['debug_level'],
-                           help = 'Print messages of severity DEBUG_LEVEL \
-                                   or higher (Default %s)'
-                                   % (defaults['debug_level'],))
-    log_group.add_argument('-L', '--log-file',
-                           action = 'store',
-                           nargs = '?',
-                           const = defaults['log_file'],
-                           default = False,
-                           help = 'Log output in LOG_FILE. If -L is specified \
+    log_group.add_argument(
+        '-D',
+        '--debug-level',
+        action='store',
+        choices=debug_choices,
+        default=defaults['debug_level'],
+        help='Print messages of severity DEBUG_LEVEL \
+                                   or higher (Default %s)' %
+        (defaults['debug_level'], ))
+    log_group.add_argument(
+        '-L',
+        '--log-file',
+        action='store',
+        nargs='?',
+        const=defaults['log_file'],
+        default=False,
+        help='Log output in LOG_FILE. If -L is specified \
                                    but LOG_FILE is omitted, %s will be used. \
                                    If the argument is omitted altogether, \
-                                   logging will not take place at all.'
-                                   % (defaults['log_file'],))
-    log_group.add_argument('-l', '--log-level',
-                           action = 'store',
-                           choices = debug_choices,
-                           default = defaults['log_level'],
-                           help = 'Log messages of severity LOG_LEVEL or \
+                                   logging will not take place at all.' %
+        (defaults['log_file'], ))
+    log_group.add_argument(
+        '-l',
+        '--log-level',
+        action='store',
+        choices=debug_choices,
+        default=defaults['log_level'],
+        help='Log messages of severity LOG_LEVEL or \
                                    higher. Only makes sense if -L is also \
-                                   specified (Default %s)'
-                                   % (defaults['log_level'],))
+                                   specified (Default %s)' %
+        (defaults['log_level'], ))
 
     gen_group = parser.add_argument_group('General Options')
-    gen_group.add_argument('-v', '--version', action = 'version',
-                           version = 'pyCCSniffer v%s' % (__version__))
-    gen_group.add_argument('-h', '--help', action = 'help',
-                           help = 'Shows this message and exits')
+    gen_group.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        version='pyCCSniffer v%s' % (__version__))
+    gen_group.add_argument(
+        '-h', '--help', action='help', help='Shows this message and exits')
 
     return parser.parse_args()
+
 
 def dump_stats():
     s = StringIO()
@@ -470,6 +505,7 @@ def dump_stats():
 
     print(s.getvalue())
 
+
 def log_init():
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
@@ -479,13 +515,13 @@ def log_init():
     logger.addHandler(ch)
 
     if args.log_file is not False:
-        fh = logging.handlers.RotatingFileHandler(filename = args.log_file,
-                                                  maxBytes = 5000000)
+        fh = logging.handlers.RotatingFileHandler(
+            filename=args.log_file, maxBytes=5000000)
         fh.setLevel(getattr(logging, args.log_level))
-        ff = logging.Formatter(
-            '%(asctime)s - %(levelname)8s - %(message)s')
+        ff = logging.Formatter('%(asctime)s - %(levelname)8s - %(message)s')
         fh.setFormatter(ff)
         logger.addHandler(fh)
+
 
 if __name__ == '__main__':
     args = arg_parser()
@@ -493,7 +529,6 @@ if __name__ == '__main__':
 
     logger.info('Started logging')
     start_datetime = datetime.now()
-
 
     packetHandler = PacketHandler()
     packetHandler.enable()
@@ -503,6 +538,7 @@ if __name__ == '__main__':
 
     # Create a list of handlers to dispatch to, NB: handlers must have a "handleSniffedPacket" method
     handlers = [packetHandler]
+
     def handlerDispatcher(timestamp, macPDU):
         """ Dispatches any received packets to all registered handlers
 
@@ -547,16 +583,17 @@ if __name__ == '__main__':
             else:
                 try:
                     # use the Windows friendly "raw_input()", instead of select()
-                   cmd = input('')
+                    cmd = input('')
 
-                   if '' != cmd:
-                        logger.debug('User input: "%s"' % (cmd,))
+                    if '' != cmd:
+                        logger.debug('User input: "%s"' % (cmd, ))
                         if cmd in ('h', '?'):
                             printHelp()
                         elif cmd == 'c':
                             # We'll only ever see this if the user asked for it, so we are
                             # running interactive. Print away
-                            print('Sniffing in channel: {:d}'.format(snifferDev.get_channel()))
+                            print('Sniffing in channel: {:d}'.format(
+                                snifferDev.get_channel()))
                         elif cmd == 'd':
                             if packetHandler.isEnabled():
                                 packetHandler.disable()
@@ -567,7 +604,7 @@ if __name__ == '__main__':
                         elif cmd == 'p':
                             logger.info('User requested print all')
                             packetHandler.printAllFrames()
-                            
+
                         elif cmd == 'q':
                             logger.info('User requested shutdown')
                             sys.exit(0)
@@ -585,11 +622,12 @@ if __name__ == '__main__':
                                 packetHandler.setAnnotation(cmd[1:].strip())
                         elif int(cmd) in range(11, 27):
                             snifferDev.set_channel(int(cmd))
-                            print('Sniffing in channel: %d' % (snifferDev.get_channel(),))
+                            print('Sniffing in channel: %d' %
+                                  (snifferDev.get_channel(), ))
                         else:
                             print("Channel must be from 11 to 26 inclusive.")
                 except ValueError:
-                    print(e)
+                    print('Unknown Command. Type h or ? for help')
                 except UnboundLocalError:
                     # Raised by command 'n' when -o was specified at command line
                     pass
@@ -600,4 +638,3 @@ if __name__ == '__main__':
             snifferDev.stop()
         dump_stats()
         sys.exit(0)
-
